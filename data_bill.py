55de463559476c767c099a22d61dc31d4c8be2ff
Jaejupro <timdarleen008@gmail.com>
2026-05-07 00:40:55 -0700

    inputs

--- data_bill.py
+++ data_bill.py
@@ -8,5 +8,11 @@ REGULAR_USER_OVERAGE_RATE_TIER_3 = 3
 
 
 # Your code goes here:
+#Ask the user fot the following inputs
+data_used = int(input("Enter the amount of data used: "))
+monthy_cost = float(input("Enter the monthly cost: "))
+premium_user = input("Do you have a premium plan? (yes/no): ")
 
-x = 1
+if data_used <= TIER_1_DATA_LIMIT_GB:
+    total_cost = monthy_cost
+    print 
